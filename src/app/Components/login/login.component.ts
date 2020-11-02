1e431548f3e65cf47d766b1a8185d5b951adea3f
ehared <eltire.hared@hotmail.com>
2020-11-02 02:07:21 -0500

    css on login/registration, usrService, login works

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { User } from 'src/app/Models/user';
+import { UserService } from 'src/app/Services/userService';
+import {share} from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  styleUrls: ['./login.component.scss', '../../../assets/scss/purpose-dark.scss'],
 })
 export class LoginComponent {
 
-  driver: 
-  {
-    email: string,
-    password: string,
-  } = {
-      email: '',
-      password: '',
-  };
+  user = new User;
+  show: boolean = false;
+  constructor(public router: Router, private userService: UserService, private toastrService: ToastrService) { }
 
-  constructor() { }
+  showPassword() {
+    this.show = !this.show;
+    var showPass = document.getElementById('password-input');
 
+    if (this.show) {
+      showPass.setAttribute('type', 'text');
+    } else {
+      showPass.setAttribute('type', 'password');
+    }
+    console.log("clicked!");
+  }
+  login(){
+    var userForm = this.userService.jsonToFormData(this.user);
+    this.userService.login(userForm).pipe(share()).subscribe((response: any)=>{
+      this.toastrService.success("Successfully logged in.","Login", {timeOut:3000,});
+      console.log("Successfully Logged in.");
+      console.log(response.data);
+    },err => {
+      console.log(err);
+    })
+  }
 }
